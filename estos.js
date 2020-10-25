54249415ed35641f96d4f12f0f447dfb614db389
Peter Simon <simon.peter.wolf@gmail.com>
2020-10-25 19:06:52 +0100

    create stores/tables and add data. query simple where

--- estos.js
+++ estos.js
@@ -1,40 +1,19 @@
 import * as eChat from './modules/mChat.js';
 
+// predefined demo data to create stores
 const g_rgChat = [
-	{
-		u8sEventCrossRefID: "jBnFffRDPlOKh",
-		u8sConversationID: "psi@estos.de;pws@estos.de",
-		iConvSequenceID: 1,
-		u8sSenderURI: "sip:psi@estos.de",
-		asnCreateTime: "2020-10-14T13:00:00.000Z", // date.toISOString()
-		asnChatMessage: {
-				u8sMessage: "hurts"
-			}
-	},
-	{
-		u8sEventCrossRefID: "",
-		u8sConversationID: "psi@estos.de;pws@estos.de",
-		iConvSequenceID: 2,
-		u8sSenderURI: "sip:psi@estos.de",
-		asnCreateTime: "2020-10-14T13:01:00.000Z",
-		asnChatMessage: {
-				u8sMessage: "wurts"
-			}
-	},
-];
-
-const g_rgConversation = [
 		{
-			u8sEventCrossRefID: "jBnFffRDPlOKh",
+			u8sConversationID: "psi@estos.de;pws@estos.de",
 			iConvSequenceID: 1,
 			u8sSenderURI: "sip:psi@estos.de",
 			asnCreateTime: "2020-10-14T13:00:00.000Z", // date.toISOString()
 			asnChatMessage: {
 					u8sMessage: "hurts"
-				}
+				},
+			u8sEventCrossRefID: "jBnFffRDPlOKh"
 		},
 		{
-			u8sEventCrossRefID: "",
+			u8sConversationID: "psi@estos.de;pws@estos.de",
 			iConvSequenceID: 2,
 			u8sSenderURI: "sip:psi@estos.de",
 			asnCreateTime: "2020-10-14T13:01:00.000Z",
@@ -42,6 +21,40 @@ const g_rgConversation = [
 					u8sMessage: "wurts"
 				}
 		},
+	];
+const g_schema = "u8sEventCrossRefID, iConvSequenceID";
+const g_stores = {
+		Cpsi_pws: g_schema,
+		Ceb_psi: g_schema
+	};
+const g_rgConversation = [
+		{
+			iConvSequenceID: 1,
+			u8sSenderURI: "sip:psi@estos.de",
+			asnCreateTime: "2020-10-14T13:00:00.000Z", // date.toISOString()
+			asnChatMessage: {
+					u8sMessage: "hurts"
+				},
+			u8sEventCrossRefID: "jBnFffRDPlOKh"
+		},
+		{
+			iConvSequenceID: 2,
+			u8sSenderURI: "sip:psi@estos.de",
+			asnCreateTime: "2020-10-14T13:01:00.000Z",
+			asnChatMessage: {
+					u8sMessage: "wurts"
+				}
+			// u8sEventCrossRefID: "jBnFffRDPlOKh" // wird dynamisch eryzeugt und haengt deshalb hinten an
+		},
+		{
+			iConvSequenceID: 3,
+			u8sSenderURI: "sip:pws@estos.de",
+			asnCreateTime: "2020-10-14T13:02:00.000Z",
+			asnChatMessage: {
+					u8sMessage: "answer"
+				},
+			u8sEventCrossRefID: "jBrAffRDPlOKh"
+		}
 	];
 
 window.addEventListener("load", function() {
@@ -54,7 +67,7 @@ window.addEventListener("load", function() {
 			});
 
 			g_rgChat.forEach(message => {
-				const sCrossRefId = message.u8sEventCrossRefID || message.iConvSequenceID.toString();
+				message.u8sEventCrossRefID = message.u8sEventCrossRefID || message.iConvSequenceID.toString();
 				db.messages.put(message).then(function(key) { // https://dexie.org/docs/Table/Table.put()
 					console.log("put:", key, "done!");
 				}).catch(function(error) {
@@ -66,16 +79,27 @@ window.addEventListener("load", function() {
 			var db = new Dexie("Conversations");
 
 			// one store per conversation
-			db.version(1).stores({
-				Cpsi_pws: "u8sEventCrossRefID, iConvSequenceID",
-				Ceb_psi: "u8sEventCrossRefID, iConvSequenceID"
-			});
-
-			const sCrossRefId = g_rgConversation[0].u8sEventCrossRefID || g_rgChat[0].iConvSequenceID.toString();
-			db.friends.put(g_rgConversation[0]).then(function(key) {
-				console.log("put:", key, "done!");
-			}).catch(function(error) {
-				console.log("error:", error);
+			// https://dexie.org/docs/Tutorial/Getting-started
+			db.version(1).stores(g_stores);
+			g_rgConversation.forEach(message => {
+				message.u8sEventCrossRefID = message.u8sEventCrossRefID || message.iConvSequenceID.toString();
+				db.Cpsi_pws.put(message).then(function(key) {
+					console.log("put:", key, "done!");
+				}).catch(function(error) {
+					console.log("error:", error);
+				});
 			});
 		});
+	document.getElementById("btnPage").addEventListener("click", function(e) {
+		var db = new Dexie("Conversations");
+
+		// one store per conversation
+		db.version(1).stores(g_stores);
+
+		db.Cpsi_pws.where("iConvSequenceID").aboveOrEqual(2).each(function (message) {
+			console.log("Found: ", message);
+		}).catch(function (error) {
+			console.error(error);
+		});
+	});
 });
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 * needs Babel transpile and sourceMaps
 */
 window.addEventListener("load", function() {
-	console.log("enter index.js::onload()");
+	// console.log("enter index.js::onload()");
 	document.getElementById("btnQuery").addEventListener("click", e => {
 			console.log("here we go from babel transpiler");
 		});
